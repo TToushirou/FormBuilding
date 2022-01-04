9daaeda8da3291d939922ff6caf599d68cf1aef9
TToushirou <46146484+TToushirou@users.noreply.github.com>
2022-01-04 05:33:05 +0000

    Update script.js

--- app/js/script.js
+++ app/js/script.js
@@ -13,23 +13,52 @@ form.addEventListener('submit', e => {
 
 function checkInputs(){
 	const firstnameValue = firstname.value.trim();
+	const lastnameValue = lastname.value.trim();
+	const emailValue = email.value.trim();
+	const passwordValue = lastname.value.trim();
 
 	if(firstnameValue === '') {
 		setErrorFor(firstname, ' First name cannot be blank');
 	} else {
 		setSuccessFor(firstname);
 	}
+
+	if(lastnameValue === '') {
+		setErrorFor(lastname, ' Last name cannot be blank');
+	} else {
+		setSuccessFor(lastname);
+	}
+
+	if(emailValue === '') {
+		setErrorFor(email, 'Email cannot be blank');
+	} else if (!isEmail(emailValue)) {
+		setErrorFor(email, 'Not a valid email');
+	} else {
+		setSuccessFor(email);
+	}
+	
+	if(passwordValue === '') {
+		setErrorFor(password, 'Password cannot be blank');
+	} else if (password.value.length < 7) {
+		setErrorFor(password, 'Less than 7 characters');
+	} else {
+		setSuccessFor(password);
+	}
 }
 
 function setErrorFor(input, message) {
 	const formc = input.parentElement;
 	const small = formc.querySelector('small');
-	formc.classname = 'formc error';
+	formc.className = 'formc error';
 	small.innerText = message;
 }
 
 function setSuccessFor(input) {
 	const formc = input.parentElement;
-	formc.classname = 'formc success';
+	formc.className = 'formc success';
+}
+
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
 
